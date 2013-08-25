a927e5f88b24b056a4cb2bab6d254bf40a1da3aa
Xavier Fernandez <xavi.fernandez@gmail.com>
2013-08-25 10:05:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails3DeviseRspecCucumber::Application.config.secret_key_base = 'b40124ea615ce024b7288ca5ffe880e47893e5ef407ed46475168674974850b5baa1b92bf54fc6b68cda8c7a4cd526ece9629abbed9d75afffc2dc96052ad2e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails3DeviseRspecCucumber::Application.config.session_store :cookie_store, key: '_rails3-devise-rspec-cucumber_session'
